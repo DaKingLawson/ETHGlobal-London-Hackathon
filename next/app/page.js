0cec713c2533f5b091a5be66d526a890d9298b97
Neel <neelk2@illinois.edu>
2024-03-16 21:06:19 +0000

    login works, state doesn't update

--- next/app/page.js
+++ next/app/page.js
@@ -1,21 +1,174 @@
 "use client";
 
 import Image from "next/image";
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import { AdvancedImage } from "@cloudinary/react";
 import { Resize, scale } from "@cloudinary/url-gen/actions/resize";
 import { Cloudinary } from "@cloudinary/url-gen";
 import LoginCard from "@/components/LoginCard";
+import { Web3Auth } from "@web3auth/modal";
+import { CHAIN_NAMESPACES, IProvider, WEB3AUTH_NETWORK } from "@web3auth/base";
+import Web3 from "web3";
+import {
+  providerToSmartAccountSigner,
+  ENTRYPOINT_ADDRESS_V06,
+} from "permissionless";
+import { signerToSafeSmartAccount } from "permissionless/accounts";
+import { EIP1193Provider, createPublicClient, http } from "viem";
+import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
+import { sepolia } from "viem/chains";
+import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
+
+const clientId =
+  "BLZgeDsQhrFbZSYgXRVmB-5R1PzjXJBeZ8hXftrqnlgW5XRYLMxNYiMUzRp3l1dDxwa0w7iAwSII_wTrWEJs4uU"; // get from https://dashboard.web3auth.io
+
+const chainConfig = {
+  chainNamespace: CHAIN_NAMESPACES.EIP155,
+  chainId: "0x14A34", // hex of 84532
+  rpcTarget: "https://sepolia.base.org",
+  // Avoid using public rpcTarget in production.
+  // Use services like Infura, Quicknode etc
+  displayName: "Base Sepolia",
+  blockExplorerUrl: "https://sepolia-explorer.base.org",
+  ticker: "ETH",
+  tickerName: "ETH",
+  logo: "https://github.com/base-org/brand-kit/blob/main/logo/symbol/Base_Symbol_Blue.svg",
+};
+
+const privateKeyProvider = new EthereumPrivateKeyProvider({
+  config: { chainConfig: chainConfig },
+});
+
+const web3auth = new Web3Auth({
+  clientId,
+  web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_DEVNET,
+  privateKeyProvider: privateKeyProvider,
+});
+
+//Objective: create multiple users to feed to maci
+
+export const publicClient = createPublicClient({
+  transport: http("https://rpc.ankr.com/eth_sepolia"),
+  chain: sepolia,
+});
+
+// Create the smart account signer from the provider and signer address
 
 export default function Home() {
   const [loggedIn, setLoggedIn] = useState(false);
+  const [provider, setProvider] = useState(null);
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        await web3auth.initModal();
+        setProvider(web3auth.provider);
+
+        if (web3auth.connected) {
+          setLoggedIn(true);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    init();
+  }, []);
+
+  const login = async () => {
+    const web3authProvider = await web3auth.connect();
+    //Create the smart account signer from the provider and signer address {The account that will be interacting with contracts on this accounts behalf}
+    const smartAccountSigner = await providerToSmartAccountSigner(
+      web3authProvider
+    ).then(console.log("hi"));
+
+    //Add a signer to the web3 auth account that was just created
+    const smartAccount = await signerToSafeSmartAccount(publicClient, {
+      signer: smartAccountSigner,
+      safeVersion: "1.4.1",
+      entryPoint: ENTRYPOINT_ADDRESS_V06,
+    });
+    setProvider(web3authProvider);
+    if (web3auth.connected) {
+      setLoggedIn(true);
+    } else {
+      console.log("Not connected :(");
+    }
+  };
+
+  const getUserInfo = async () => {
+    const user = await web3auth.getUserInfo();
+    uiConsole(user);
+  };
+
+  const logout = async () => {
+    await web3auth.logout();
+    setProvider(null);
+    setLoggedIn(false);
+    uiConsole("logged out");
+  };
+
+  const getAccounts = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const web3 = new Web3(provider);
+
+    // Get user's Ethereum public address
+    const address = await web3.eth.getAccounts();
+    uiConsole(address);
+  };
+
+  const getBalance = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const web3 = new Web3(provider);
+
+    // Get user's Ethereum public address
+    const address = (await web3.eth.getAccounts())[0];
+
+    // Get user's balance in ether
+    const balance = web3.utils.fromWei(
+      await web3.eth.getBalance(address), // Balance is in wei
+      "ether"
+    );
+    uiConsole(balance);
+  };
+
+  const signMessage = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const web3 = new Web3(provider);
+
+    // Get user's Ethereum public address
+    const fromAddress = (await web3.eth.getAccounts())[0];
+
+    const originalMessage = "YOUR_MESSAGE";
+
+    // Sign the message
+    const signedMessage = await web3.eth.personal.sign(
+      originalMessage,
+      fromAddress,
+      "test password!" // configure your own password here.
+    );
+    uiConsole(signedMessage);
+  };
+
+  function uiConsole(...args) {
+    const el = document.querySelector("#console>p");
+    if (el) {
+      el.innerHTML = JSON.stringify(args || {}, null, 2);
+    }
+    console.log(...args);
+  }
+
   const description =
     "Imagine a world where communities come together effortlessly to support their own.";
-
-  const handleClick = () => {
-    console.log("hi");
-    setLoggedIn(!loggedIn);
-  };
 
   const cld = new Cloudinary({
     cloud: {
@@ -38,12 +191,16 @@ export default function Home() {
       <LoginCard
         heading="Hey, Neighbor!"
         description={description}
-        handleClick={handleClick}
+        handleClick={login}
       />
     </div>
   );
 
-  const loggedInView = <div></div>;
+  const loggedInView = (
+    <div className="bg-eggshell">
+      <butto onClick={logout}>Logout</butto>
+    </div>
+  );
 
   return loggedIn ? loggedInView : loggedOutView;
 }
--- next/components/LoginButton.jsx
+++ next/components/LoginButton.jsx
@@ -5,11 +5,11 @@ const LoginButton = ({ provider, styles, handleClick }) => {
     apple: (
       <button
         type="button"
-        class="text-white bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30 mr-2 mb-2"
+        className="text-white bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30 mr-2 mb-2"
         onClick={handleClick}
       >
         <svg
-          class="mr-2 -ml-1 w-5 h-5"
+          className="mr-2 -ml-1 w-5 h-5"
           aria-hidden="true"
           focusable="false"
           data-prefix="fab"
@@ -29,11 +29,11 @@ const LoginButton = ({ provider, styles, handleClick }) => {
     google: (
       <button
         type="button"
-        class="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2"
+        className="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2"
         onClick={handleClick}
       >
         <svg
-          class="mr-2 -ml-1 w-4 h-4"
+          className="mr-2 -ml-1 w-4 h-4"
           aria-hidden="true"
           focusable="false"
           data-prefix="fab"

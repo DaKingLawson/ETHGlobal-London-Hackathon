23bef1ce30d3254bde7000891b55ca8c356958cd
Neel <neelk2@illinois.edu>
2024-03-16 19:50:21 +0000

    next app works partially

--- next/tailwind.config.js
+++ next/tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        coinBlue: "#2050F5",
+        eggshell: "#F4F2ED",
+        almostBlack: "#14181C",
+      },
     },
   },
   plugins: [],

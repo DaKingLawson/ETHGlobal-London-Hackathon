9b0e9ca6fdd37323d213b4b6d902cc4245533633
Neel <neelk2@illinois.edu>
2024-03-16 18:11:35 +0000

    add state to google login

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import React from "react";
+import { React, useState } from "react";
 import { AdvancedImage } from "@cloudinary/react";
 import { Resize, scale } from "@cloudinary/url-gen/actions/resize";
 import { Cloudinary } from "@cloudinary/url-gen";
 import LoginCard from "./components/LoginCard";
+import { init } from "./utils/login";
+import "./App.css";
+// import RPC from './ethersRPC' // for using ethers.js
 
 const App = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const description =
+    "Imagine a world where communities come together effortlessly to support their own.";
+
+  const handleClick = () => {
+    setLoggedIn(!loggedIn);
+  };
+
   const cld = new Cloudinary({
     cloud: {
       cloudName: "dosguo3sg",
     },
   });
-
-  const description =
-    "Imagine a world where communities come together effortlessly to support their own.";
-
   const myImage = cld.image("hackathon/cnz5nwtobk1xfebzd3ko");
   myImage.resize(Resize.scale().width(100));
-  return (
+
+  const loggedOutView = (
     <div className="flex flex-row h-screen bg-eggshell">
       <div className="flex min-w-[50%]">
         <AdvancedImage
@@ -26,9 +34,17 @@ const App = () => {
         />
       </div>
 
-      <LoginCard heading="Hey, Neighbor!" description={description} />
+      <LoginCard
+        heading="Hey, Neighbor!"
+        description={description}
+        handleClick={handleClick}
+      />
     </div>
   );
+
+  const loggedInView = <div></div>;
+
+  return loggedIn ? loggedInView : loggedOutView;
 };
 
 export default App;

cac66ddaab0f861ae4b3e9bc3f10924a3d445054
Neel <neelk2@illinois.edu>
2024-03-16 13:33:22 +0000

    react setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { AdvancedImage } from "@cloudinary/react";
+import { Resize, scale } from "@cloudinary/url-gen/actions/resize";
+import { Cloudinary } from "@cloudinary/url-gen";
+import LoginCard from "./components/LoginCard";
 
-function App() {
+const App = () => {
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: "dosguo3sg",
+    },
+  });
+  const myImage = cld.image("hackathon/cnz5nwtobk1xfebzd3ko");
+  myImage.resize(Resize.scale().width(100));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-row h-screen bg-eggshell">
+      <div className="flex min-w-[50%]">
+        <AdvancedImage
+          cldImg={myImage}
+          alt="Your Image Description"
+          className="w-full h-full object-cover"
+        />
+      </div>
+
+      <LoginCard heading="Hey, Neighbor!" />
     </div>
   );
-}
+};
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        coinBlue: "#2050F5",
+        eggshell: "#F4F2ED",
+      },
+    },
+  },
+  plugins: [],
+};

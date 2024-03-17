891227e78cf1fdffc7e2a9c38a6251246d5c07fc
Neel <neelk2@illinois.edu>
2024-03-17 00:45:09 +0000

    private key setup but on client

--- next/app/page.js
+++ next/app/page.js
@@ -24,8 +24,6 @@ import {
   privateKeyToSafeSmartAccount,
 } from "permissionless/accounts";
 import { writeFileSync } from "fs";
-import { privateKeyToAccount } from "viem/accounts";
-import { pimlicoBundlerClient } from "../pimlicoBundlerClient";
 
 const clientId =
   "BLZgeDsQhrFbZSYgXRVmB-5R1PzjXJBeZ8hXftrqnlgW5XRYLMxNYiMUzRp3l1dDxwa0w7iAwSII_wTrWEJs4uU"; // get from https://dashboard.web3auth.io
@@ -63,16 +61,6 @@ export const publicClient = createPublicClient({
   chain: baseSepolia,
 });
 
-//generate a private key
-const privateKey =
-  process.env.PRIVATE_KEY ??
-  (() => {
-    const pk = generatePrivateKey();
-    writeFileSync(".env", `PRIVATE_KEY=${pk}`);
-    return pk;
-  })();
-
-//creates a SmartAccount from private key
 // Create the smart account signer from the provider and signer address
 
 export default function Home() {
@@ -97,31 +85,52 @@ export default function Home() {
   }, []);
 
   const login = async () => {
-    const web3authProvider = await web3auth.connect();
-    //Create the smart account signer from the provider and signer address {The account that will be interacting with contracts on this accounts behalf}
-    // try {
-    // } catch (e) {
-    //   console.log(e.stack);
-    // }
-    const smartAccountSigner = await providerToSmartAccountSigner(
-      web3authProvider
-    ).then(console.log);
+    //generate a private key
+    const privateKey =
+      process.env.PRIVATE_KEY ??
+      (() => {
+        const pk = generatePrivateKey();
+        writeFileSync(".env", `PRIVATE_KEY=${pk}`);
+        return pk;
+      })();
 
-    //Add a signer to the web3 auth account that was just created
-    const smartAccount = await signerToSafeSmartAccount(publicClient, {
-      signer: smartAccountSigner,
-      safeVersion: "1.4.1",
+    const signer = privateKeyToAccount(privateKey);
+
+    //creates a Safe Account from signer
+
+    const safeAccount = await signerToSafeSmartAccount(publicClient, {
       entryPoint: ENTRYPOINT_ADDRESS_V06,
+      signer: signer,
+      saltNonce: 0n, // optional
+      safeVersion: "1.4.1",
+      // address: "0x...", // optional, only if you are using an already created account
     }).then(console.log);
-    setProvider(web3authProvider);
-    if (web3auth.connected) {
-      console.log("here");
-      setLoggedIn(true);
-      const userInfo = await getUserInfo();
-      console.log(userInfo);
-    } else {
-      console.log("Not connected :(");
-    }
+
+    // const web3authProvider = await web3auth.connect();
+    // //Create the smart account signer from the provider and signer address {The account that will be interacting with contracts on this accounts behalf}
+    // // try {
+    // // } catch (e) {
+    // //   console.log(e.stack);
+    // // }
+    // const smartAccountSigner = await providerToSmartAccountSigner(
+    //   web3authProvider
+    // ).then(console.log);
+
+    // //Add a signer to the web3 auth account that was just created
+    // const smartAccount = await signerToSafeSmartAccount(publicClient, {
+    //   signer: smartAccountSigner,
+    //   safeVersion: "1.4.1",
+    //   entryPoint: ENTRYPOINT_ADDRESS_V06,
+    // }).then(console.log);
+    // setProvider(web3authProvider);
+    // if (web3auth.connected) {
+    //   console.log("here");
+    //   setLoggedIn(true);
+    //   const userInfo = await getUserInfo();
+    //   console.log(userInfo);
+    // } else {
+    //   console.log("Not connected :(");
+    // }
   };
 
   const getUserInfo = async () => {
